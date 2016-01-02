d24580361d51e814f260719b351df53bfe9a7fa2
bryan_mac <levuthanhbinh@gmail.com>
2016-01-02 20:33:39 +0700

    Change menu

---
+++ js/menu_cyo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bryanle on 12/31/15.
+ */
+/*$(document).ready( function() {
+
+    $('.cyo-masonry').masonry({
+        itemSelector: '.cyo-item',
+        columnWidth: 200
+    });
+
+});*/
