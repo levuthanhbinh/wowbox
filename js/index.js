91abe755c1161a819cf0c1d63e71a01faed7f287
bryan_mac <levuthanhbinh@gmail.com>
2016-01-03 17:08:18 +0700

    Start edit VN version

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 jQuery(document).ready(function () {
-    /******************************************
-     -    PREPARE PLACEHOLDER FOR SLIDER    -
-     ******************************************/
-
-
     var setREVStartSize = function () {
         var tpopt = new Object();
         tpopt.startwidth = 1000;
-        tpopt.startheight = 700;
+        tpopt.startheight = 800;
         tpopt.container = jQuery('#slider-1');
         tpopt.fullScreen = "off";
         tpopt.forceFullWidth = "on";
@@ -61,85 +56,90 @@ jQuery(document).ready(function () {
     setREVStartSize();
 
     var tpj = jQuery;
-    tpj.noConflict();
+    //tpj.noConflict();
     var slider1;
 
     tpj(document).ready(function () {
+        if (tpj('#slider-1').revolution == undefined){}
+        else {
+            slider1 = tpj('#slider-1').show().revolution({
+                dottedOverlay: "none",
+                delay: 5000,
+                startwidth: 1000,
+                startheight: 800,
+                hideThumbs: 200,
 
-        if (tpj('#slider-1').revolution == undefined)
-            revslider_showDoubleJqueryError('#rev_slider_6_1');
-        else
-            slider1 = tpj('#slider-1').show().revolution(
-                {
-                    dottedOverlay: "none",
-                    delay: 5000,
-                    startwidth: 1000,
-                    startheight: 800,
-                    hideThumbs: 200,
+                thumbWidth: 100,
+                thumbHeight: 50,
+                thumbAmount: 4,
 
-                    thumbWidth: 100,
-                    thumbHeight: 50,
-                    thumbAmount: 4,
+                navigationType: "bullet",
+                navigationArrows: "solo",
+                navigationStyle: "square",
 
-                    navigationType: "bullet",
-                    navigationArrows: "solo",
-                    navigationStyle: "square",
+                touchenabled: "on",
+                onHoverStop: "off",
 
-                    touchenabled: "on",
-                    onHoverStop: "off",
+                swipe_velocity: 0.7,
+                swipe_min_touches: 1,
+                swipe_max_touches: 1,
+                drag_block_vertical: false,
 
-                    swipe_velocity: 0.7,
-                    swipe_min_touches: 1,
-                    swipe_max_touches: 1,
-                    drag_block_vertical: false,
+                parallax: "mouse",
+                parallaxBgFreeze: "on",
+                parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
 
-                    parallax: "mouse",
-                    parallaxBgFreeze: "on",
-                    parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+                keyboardNavigation: "off",
 
-                    keyboardNavigation: "off",
+                navigationHAlign: "center",
+                navigationVAlign: "bottom",
+                navigationHOffset: 0,
+                navigationVOffset: 40,
 
-                    navigationHAlign: "center",
-                    navigationVAlign: "bottom",
-                    navigationHOffset: 0,
-                    navigationVOffset: 40,
+                soloArrowLeftHalign: "left",
+                soloArrowLeftValign: "center",
+                soloArrowLeftHOffset: 20,
+                soloArrowLeftVOffset: -30,
 
-                    soloArrowLeftHalign: "left",
-                    soloArrowLeftValign: "center",
-                    soloArrowLeftHOffset: 20,
-                    soloArrowLeftVOffset: -30,
+                soloArrowRightHalign: "right",
+                soloArrowRightValign: "center",
+                soloArrowRightHOffset: 20,
+                soloArrowRightVOffset: -30,
 
-                    soloArrowRightHalign: "right",
-                    soloArrowRightValign: "center",
-                    soloArrowRightHOffset: 20,
-                    soloArrowRightVOffset: -30,
+                shadow: 0,
+                fullWidth: "on",
+                fullScreen: "off",
 
-                    shadow: 0,
-                    fullWidth: "on",
-                    fullScreen: "off",
+                spinner: "spinner4",
 
-                    spinner: "spinner4",
+                stopLoop: "off",
+                stopAfterLoops: -1,
+                stopAtSlide: -1,
 
-                    stopLoop: "off",
-                    stopAfterLoops: -1,
-                    stopAtSlide: -1,
+                shuffle: "off",
 
-                    shuffle: "off",
+                autoHeight: "off",
+                forceFullWidth: "on",
 
-                    autoHeight: "off",
-                    forceFullWidth: "off",
+                hideThumbsOnMobile: "off",
+                hideNavDelayOnMobile: 1500,
+                hideBulletsOnMobile: "off",
+                hideArrowsOnMobile: "off",
+                hideThumbsUnderResolution: 0,
 
-                    hideThumbsOnMobile: "off",
-                    hideNavDelayOnMobile: 1500,
-                    hideBulletsOnMobile: "off",
-                    hideArrowsOnMobile: "off",
-                    hideThumbsUnderResolution: 0,
-
-                    hideSliderAtLimit: 0,
-                    hideCaptionAtLimit: 0,
-                    hideAllCaptionAtLilmit: 0,
-                    startWithSlide: 0
-                });
+                hideSliderAtLimit: 0,
+                hideCaptionAtLimit: 0,
+                hideAllCaptionAtLilmit: 0,
+                startWithSlide: 0
+            });
+        }
     });
-    /*ready*/
+
+    $('#section-location-map').click(function () {
+        $('#section-location-map').css("pointer-events", "auto");
+    });
+
+    $('#section-location-map').mouseleave(function() {
+        $('#section-location-map').css("pointer-events", "none");
+    });
 });
